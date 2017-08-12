6a3dca8d165e6f24537155814c8ec3fe18512da0
EduardoHD94 <eherrerad94@gmail.com>
2017-08-12 18:16:23 -0500

    Configuring server

--- src/server.js
+++ src/server.js
@@ -1,16 +1,31 @@
 import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
-import "express-group-routes";
-
+import config from "./config/db";
+import 'express-group-routes';
+import ROUTES from "./routes";
 
 const PORT = process.env.PORT || 3000;
 const app = express();
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.disable("x-powered-by");
+app.use(bodyParser.json());// Parse the request body into a more usabel object
+app.use(bodyParser.urlencoded({ extended: true }));//Parse the request body into a www-url-encoded
+app.disable("x-powered-by"); //Disable x-powered-by in rest
 
 
+
+//Database Connection
+mongoose.Promise = global.Promise;
+mongoose.connection
+    .openUri(config.localhost) //here comes the db url //mongoose.connect(config.localhost)
+    .then(() => {
+        console.log("Connected to db successfully ");
+    }).catch((err) => {
+        console.log("An error ocurred ", err);
+        process.exit(1);
+    });
+
+
+app.use('/',ROUTES);
 
 app.listen(PORT, (err) => {
     if (!err)
